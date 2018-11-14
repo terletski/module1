5a3cad36498dbd772dfb0192fd1e771788c20d9e
Yauheni Tsiarletski <yauheni_tsiarletski@epam.com>
2018-11-14 11:20:06 +0300

    commit 1

---
+++ task1/Task1.js
@@ -0,0 +1,22 @@
+//requirements for use:
+console.log ('Valid file extensions: txt, html, xml and encoded UTF-8.' + '\n');
+
+let readFileAndPrintEvenLines = () => {
+    console.log('Even lines: ')
+    let fs = require('fs');
+    let arr;
+       fs.readFile('Text.txt', (err, data) => { //read file 
+        if (err) {throw err}
+        arr = data.toString();
+        arr = arr.split('\n'); //split file into lines
+        
+        for (let i = 0; i<arr.length; i++) {      //print even lines
+            if ([i]%2==1){console.log(arr[i]);
+            }
+        } 
+    });
+}
+ 
+readFileAndPrintEvenLines();
+
+
