1b9ac3f126ff92247c15cdd65ad103dd4f8ac30d
Yauheni Tsiarletski <yauheni_tsiarletski@epam.com>
2018-11-15 16:23:14 +0300

    fixed task2.js

--- task2/task2.js
+++ task2/task2.js
@@ -10,37 +10,37 @@ let fs = require('fs');
     let data = JSON.parse(content);
 
     typeof data.flag === 'boolean' || data.flag === true ? console.log('OK') : 
-    fs.writeFileSync('Log.txt', 'patameter "flag" does not satisfy the condition: true' + '\n');
+    fs.writeFileSync('Log.txt', "flag : " + data.flag + '\n');
     
     typeof data.myPromises === 'object' ? console.log('OK') :
-    fs.appendFileSync('Log.txt', 'patameter "myPromises" does not satisfy the condition: {"1":2}' + '\n');
+    fs.appendFileSync('Log.txt', "myPromises : " + data.myPromises + '\n');
   
     typeof data.element === 'object' ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "element" does not satisfy the condition: {"array":[]}' + '\n');
+    fs.appendFileSync('Log.txt', "patameter : " + data.element + '\n');
 
     data.screenshot === 'null' ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "screenshot" does not satisfy the condition: null' + '\n');
+    fs.appendFileSync('Log.txt', "screenshot : " + data.screenshot + '\n');
 
     typeof data.elementText === 'string' ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "elementText" does not satisfy the condition: string' + '\n');
+    fs.appendFileSync('Log.txt', "elementText : " + data.elementText + '\n');
 
     typeof data.allElementsText === 'string' ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "allElementsText" does not satisfy the condition: tant' + '\n');
+    fs.appendFileSync('Log.txt', "allElementsText : " + data.allElementsText + '\n');
 
     typeof data.counter === 'number' || data.counter === -1 ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "counter" does not satisfy the condition: -1' + '\n');
+    fs.appendFileSync('Log.txt', "counter : " + data.counter + '\n');
 
     typeof data.config === 'string' || data.config === 'timer' ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "config" does not satisfy the condition: timer' + '\n');
+    fs.appendFileSync('Log.txt', "config : " + data.config + '\n');
 
     data.const === 'FIRST' ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "const" does not satisfy the condition: FIRST' + '\n');
+    fs.appendFileSync('Log.txt', "const : " + data.const + '\n');
 
     typeof data.parameters === 'object' ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "parameter" does not satisfy the condition: {}' + '\n');
+    fs.appendFileSync('Log.txt', "parameter : " + data.parameters  + '\n');
      
     typeof data.description === 'string' || data.description === 'qwertyuiopasdfghjkl' ? console.log('OK') : 
-    fs.appendFileSync('Log.txt', 'patameter "description" does not satisfy the condition: qwertyuiopasdfghjkl' + '\n');  
+    fs.appendFileSync('Log.txt', "description : " + data.description + '\n');  
 }
 
 checkConditions();
