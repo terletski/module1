e1c90cc74175a15e0dadd088c4f2faa178252752
Yauheni Tsiarletski <yauheni_tsiarletski@epam.com>
2018-11-16 11:55:22 +0300

    refacoring task1.js

--- task1/Task1.js
+++ task1/Task1.js
@@ -1,13 +1,14 @@
 //requirements for use:
-console.log ('Valid file extensions: txt, html, xml and encoded UTF-8.' + '\n');
+console.log ('Valid file extensions: txt, html, xml and encoded UTF-8.');
+console.log ('Enter the file name you want to read as the function <readFileAndPrintEvenLines> argument.');
 
-let readFileAndPrintEvenLines = () => {
+let readFileAndPrintEvenLines = file => {
     console.log('Even lines: ')
     let fs = require('fs');
     let arr;
-       fs.readFile('Text.txt', (err, data) => { //read file 
-        if (err) {throw err}
-        arr = data.toString().split('\n');//split file into lines
+       fs.readFile(file, (err, data) => { //read file 
+        if (err) throw err;
+        arr = data.toString().split('\n');   //split file into lines
                 
         for (let i = 0; i<arr.length; i++) {      //print even lines
             if (i%2===1){console.log(arr[i]);
@@ -16,6 +17,6 @@ let readFileAndPrintEvenLines = () => {
     });
 }
  
-readFileAndPrintEvenLines();
+readFileAndPrintEvenLines('Text.txt');
 
 
