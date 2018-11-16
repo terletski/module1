f22b48ccbf14ef05bb8e5aaa042dbfe45670c920
Yauheni Tsiarletski <yauheni_tsiarletski@epam.com>
2018-11-16 11:27:16 +0300

    Fixed task2.js

--- task2/task2.js
+++ task2/task2.js
@@ -3,13 +3,13 @@ console.log ('Valid file extensions: - json, encoded: UTF8.' + '\n');
 
 let checkConditions = () => {
 
-let fs = require('fs');
-    
-    let content = fs.readFileSync('1400528_4.json', 'utf8');
-              
+    let json_file = '1400528_4.json'; 
+    let fs = require('fs');
+    let fileExists = require('file-exists');  
+    let content = fs.readFileSync(json_file, 'utf8');         
     let data = JSON.parse(content);
 
-    typeof data.flag === 'boolean' || data.flag === true ? console.log('OK') : 
+    typeof data.flag === 'boolean' && data.flag === true ? console.log('OK') : 
     fs.writeFileSync('Log.txt', "flag : " + data.flag + '\n');
     
     typeof data.myPromises === 'object' ? console.log('OK') :
@@ -27,22 +27,26 @@ let fs = require('fs');
     typeof data.allElementsText === 'string' ? console.log('OK') : 
     fs.appendFileSync('Log.txt', "allElementsText : " + data.allElementsText + '\n');
 
-    typeof data.counter === 'number' || data.counter === -1 ? console.log('OK') : 
+    typeof data.counter === 'number' && data.counter < 0 ? console.log('OK') : 
     fs.appendFileSync('Log.txt', "counter : " + data.counter + '\n');
 
-    typeof data.config === 'string' || data.config === 'timer' ? console.log('OK') : 
+    typeof data.config === 'string' && data.config === 'timer' ? console.log('OK') : 
     fs.appendFileSync('Log.txt', "config : " + data.config + '\n');
 
-    data.const === 'FIRST' ? console.log('OK') : 
+    typeof data.const === 'string' && data.const === 'FIRST' ? console.log('OK') : 
     fs.appendFileSync('Log.txt', "const : " + data.const + '\n');
-
+    
     typeof data.parameters === 'object' ? console.log('OK') : 
     fs.appendFileSync('Log.txt', "parameter : " + data.parameters  + '\n');
      
     typeof data.description === 'string' || data.description === 'qwertyuiopasdfghjkl' ? console.log('OK') : 
     fs.appendFileSync('Log.txt', "description : " + data.description + '\n');  
-}
 
+    // check for Log.txt existence   
+    fileExists.sync('Log.txt') === true ? console.log("Log.txt was created") : 
+    console.log("Log.txt wasn't created");      
+}
+   
 checkConditions();
   
 
