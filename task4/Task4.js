a7696e09d4c88103d40b3448d872ab6c322f1b9f
terletski <39872695+terletski@users.noreply.github.com>
2018-11-19 17:30:26 +0300

    fixed task4

--- task4/Task4.js
+++ task4/Task4.js
@@ -1,14 +1,10 @@
-//requirements for use:
-console.log('Enter the country you want to find as name variable.')
-
 const request = require('request-promise');
-const uri = 'http://services.groupkt.com/country/get/all'
 
 let name = 'Belarus';
 
 const options = {
   method: 'GET',
-  uri: uri
+  uri: 'http://services.groupkt.com/country/get/all'
 }
 
 request(options)
