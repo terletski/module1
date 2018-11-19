e0a70bb91d85613335df19622a865c61ab7e4e37
terletski <39872695+terletski@users.noreply.github.com>
2018-11-19 17:29:24 +0300

    fixed task2

--- task2/Task2.js
+++ task2/Task2.js
@@ -1,9 +1,9 @@
-//requirements for use:
-console.log ('Valid file extensions: - json, encoded: UTF8.');
-console.log ('Enter the file name you want to read as the function <checkConditions> argument.');
-
 const fs = require('fs');
 const fileExists = require('file-exists');
+
+//requirements for use:
+console.log('Valid file extensions: - json, encoded: UTF8.');
+console.log('Enter the file name you want to read as the function <checkConditions> argument.');
 
 let log_file = {};
 let wrongProperties = 0;
@@ -20,32 +20,42 @@ checkConditions = json_file => {
 }
 
 readJsonFileAndWriteLog = json_file => {
-    let content = fs.readFileSync(json_file, 'utf8');         
+    let content = fs.readFileSync(json_file, 'utf8');
     let data = JSON.parse(content);
-  
+
     if (typeof data.flag !== 'boolean') writeLogFile('flag', data.flag);
+
     if (typeof data.myPromises !== 'object') writeLogFile('myPromises', data.myPromises);
+
     if (typeof data.element !== 'object') writeLogFile('element', data.element);
+
     if (data.screenshot !== 'null') writeLogFile('screenshot', data.screenshot);
+
     if (typeof data.elementText !== 'string') writeLogFile('elementText', data.elementText);
+
     if (data.allElementsText !== 'tant') writeLogFile('allElementsText', data.allElementsText);
+
     if (typeof data.counter !== 'number' && data.counter < 0) writeLogFile('counter', data.counter);
+
     if (typeof data.config !== 'string' && data.config === 'timer') writeLogFile('config', data.config);
+
     if (!/FiRst/i.test(data.const)) writeLogFile('const', data.const);
-    if (typeof data.parameters !== 'object') writeLogFile('parameters', data.parameters); 
-    if (typeof data.description !== 'string' && data.description.length === 19) writeLogFile('description', data.description); 
-     
+
+    if (typeof data.parameters !== 'object') writeLogFile('parameters', data.parameters);
+    
+    if (typeof data.description !== 'string' && data.description.length === 19) writeLogFile('description', data.description);
+
     if (wrongProperties > 0) {
         fs.writeFile('Log.txt', JSON.stringify(log_file), 'utf8', () => {
             console.log('Log.txt file was created.');
-            })
-    } 
-    else {console.log('OK')}
+        })
+    }
+    else { console.log('OK') }
 }
 
 function writeLogFile(property, value) {
     log_file[property] = value;
-    wrongProperties++ ;
+    wrongProperties++;
 }
 
 checkConditions('1400528_4.json')
