86402a71f680f47bf36472117f1d7a6952ca87ac
Yauheni Tsiarletski <yauheni_tsiarletski@epam.com>
2018-11-20 11:04:16 +0300

    update

--- task4/Task4.js
+++ task4/Task4.js
@@ -1,24 +1,23 @@
 const request = require('request-promise');
 
-let name = 'Belarus';
-
 const options = {
   method: 'GET',
   uri: 'http://services.groupkt.com/country/get/all'
 }
 
 request(options)
-  .then (function (response) {
+  .then(function (response) {
     let json_obj = JSON.parse(response);
     let country = json_obj.RestResponse.result;
+    const name = 'Belarus';
 
-      for (let prop in country){
-        if (country[prop].name === name) {
-          console.log(country[prop]);
-        } 
+    for (let prop in country) {
+      if (country[prop].name === name) {
+        console.log(country[prop]);
       }
-    })                    
-  .catch(function(err) { 
+    }
+  })
+  .catch(function (err) {
     console.log(err);
   });
-  
+
